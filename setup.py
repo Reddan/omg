5ef340d1314b05b5d24eb5eccec68b5cb5ecd9f3
Hampus <hampus@hallman.eu>
2019-04-09 19:50:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name='omg',
+  packages=['omg'],
+  version='1.0.1',
+  author='Hampus Hallman',
+  author_email='me@hampushallman.com',
+  url='https://github.com/Reddan/omg',
+  license='MIT',
+  entry_points = {
+    'console_scripts': ['omg = omg.__main__:main']
+  },
+  install_requires=[
+    'watchdog',
+    'termcolor',
+  ],
+  python_requires='~=3.6',
+)
